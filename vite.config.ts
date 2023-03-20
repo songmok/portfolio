162709d699099b1750e84bb6996ff44fee931663
songmok <www.songmokseed@gmail.com>
2023-03-20 09:55:14 +0900

    base 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://songmok.github.io/portfolio/",
   plugins: [react(), tsconfigPaths()],
   //build 시에 모든 console.log를 제거
   build: {
